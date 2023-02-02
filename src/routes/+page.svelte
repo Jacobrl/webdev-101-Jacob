bf9a1b059dc16184f9825e770acb12b223cdd0ba
Jacob L <jrl359@cornell.edu>
2023-02-02 18:27:09 -0500

    Update: Added Compnent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-Jacob
+<button class="btn btn-primary">Button</button>
+<div class="badge badge-secondary">secondary</div>
+<input type="range" min="0" max="100" value="40" class="range range-primary" />
+
 
