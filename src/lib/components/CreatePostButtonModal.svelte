a2ad0caca37ff14836b20774ab65b2ee6d21f9a2
Jacob L <jrl359@cornell.edu>
2023-02-19 18:16:13 -0500

    Update: Compelted post, page, and create post button modal svetle.

--- src/lib/components/CreatePostButtonModal.svelte
+++ src/lib/components/CreatePostButtonModal.svelte
@@ -1,61 +1,49 @@
-<script lang="ts">
-	import IconAdd from '$lib/icons/IconAdd.svelte';
+<script>
 	import { currentUser, pb } from '$lib/pocketbase';
+	let caption = '';
 
 	const formData = new FormData();
-	let caption: string = '';
+	const handleFileChange = (event) => {
+		for (let file of event.target.files) {
+			console.log(file);
+			formData.append('photo', file);
+		}
+	};
 
 	const handleCreatePost = async () => {
 		formData.append('caption', caption);
-		formData.append('user', $currentUser?.id as string);
+		formData.append('user', $currentUser?.id);
 		await pb.collection('posts').create(formData);
 		caption = '';
-	};
-
-	const handleFileChange = (event: any) => {
-		for (let file of event?.target?.files) {
-			formData.append('photos', file);
-		}
 	};
 </script>
 
 {#if $currentUser}
 	<div class="flex justify-center">
-		<label tabindex="0" for="modal-create-post" class="btn-primary btn gap-2">
-			<IconAdd />Create post
-		</label>
-	</div>
-{/if}
+		<!-- The button to open modal -->
+		<label for="my-modal-4" class="btn">Create Post</label>
 
-<!-- create post modal -->
-<input type="checkbox" id="modal-create-post" class="modal-toggle" />
-<label for="modal-create-post" class="modal cursor-pointer">
-	<label class="modal-box relative w-11/12 max-w-sm" for="">
-		<form on:submit|preventDefault>
-			<div class="flex flex-col gap-3">
-				<h3 class="p-0 text-xl font-medium">Create post</h3>
+		<!-- Put this part before </body> tag -->
+		<input type="checkbox" id="my-modal-4" class="modal-toggle" />
+		<div class="modal">
+			<div class="modal-box relative space-y-4">
+				<label for="my-modal-4" class="btn-sm btn-circle btn absolute right-2 top-2">✕</label>
+				<h3 class="text-lg font-bold">Create Post</h3>
 				<div class="form-control">
-					<label class="label" for="caption">Caption</label>
-					<textarea
-						name="caption"
-						class="textarea-bordered textarea h-24 w-full"
-						bind:value={caption}
-					/>
+					<label class="label">
+						<span class="label-text">Your bio</span>
+					</label>
+					<textarea class="textarea-bordered textarea h-24" bind:value={caption} />
 				</div>
 				<input
 					type="file"
 					class="file-input-bordered file-input w-full max-w-xs"
 					on:change={handleFileChange}
-					multiple
 				/>
-				<label
-					for="modal-create-post"
-					class="btn-primary btn-block btn"
-					on:click={handleCreatePost}
+				<label for="my-modal-4" class="btn-primary btn-block btn" on:click={handleCreatePost}>
+					Create Post</label
 				>
-					Create post
-				</label>
 			</div>
-		</form>
-	</label>
-</label>
+		</div>
+	</div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,7 @@
 <script>
-	import { currentUser, pb } from '$lib/pocketbase';
-	let caption = '';
-
-	const formData = new FormData();
-	const handleFileChange = (event) => {
-		for (let file of event.target.files) {
-			console.log(file);
-			formData.append('photo', file);
-		}
-	};
-
-	const handleCreatePost = async () => {
-		formData.append('caption', caption);
-		formData.append('user', $currentUser?.id);
-		await pb.collection('posts').create(formData);
-		caption = '';
-	};
+	import CreatePostButtonModal from '$lib/components/CreatePostButtonModal.svelte';
+	import Posts from '$lib/components/Posts.svelte';
 </script>
 
-{#if $currentUser}
-	<div class="flex justify-center">
-		<!-- The button to open modal -->
-		<label for="my-modal-4" class="btn">Create Post</label>
-
-		<!-- Put this part before </body> tag -->
-		<input type="checkbox" id="my-modal-4" class="modal-toggle" />
-		<div class="modal">
-			<div class="modal-box relative space-y-4">
-				<label for="my-modal-4" class="btn-sm btn-circle btn absolute right-2 top-2">✕</label>
-				<h3 class="text-lg font-bold">Create Post</h3>
-				<div class="form-control">
-					<label class="label">
-						<span class="label-text">Your bio</span>
-					</label>
-					<textarea class="textarea-bordered textarea h-24" bind:value={caption} />
-				</div>
-				<input
-					type="file"
-					class="file-input-bordered file-input w-full max-w-xs"
-					on:change={handleFileChange}
-				/>
-				<label for="my-modal-4" class="btn-primary btn-block btn" on:click={handleCreatePost}>
-					Create Post</label
-				>
-			</div>
-		</div>
-	</div>
-{/if}
+<CreatePostButtonModal />
+<Posts />
